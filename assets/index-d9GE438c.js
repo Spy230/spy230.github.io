36be95a7cbd139a3964cc8ee351fc63f37cb6699
Spy <shesterikovon@gmail.com>
2026-03-03 21:00:02 +0300

    Updates

--- assets/index-d9GE438c.js
+++
@@ -1,9 +0,0 @@
-import{r as f,a as m}from"./vendor-jVyfcstf.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();var d={exports:{}},l={};/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var y=f,x=Symbol.for("react.element"),_=Symbol.for("react.fragment"),h=Object.prototype.hasOwnProperty,v=y.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,O={key:!0,ref:!0,__self:!0,__source:!0};function a(s,t,i){var o,e={},r=null,n=null;i!==void 0&&(r=""+i),t.key!==void 0&&(r=""+t.key),t.ref!==void 0&&(n=t.ref);for(o in t)h.call(t,o)&&!O.hasOwnProperty(o)&&(e[o]=t[o]);if(s&&s.defaultProps)for(o in t=s.defaultProps,t)e[o]===void 0&&(e[o]=t[o]);return{$$typeof:x,type:s,key:r,ref:n,props:e,_owner:v.current}}l.Fragment=_;l.jsx=a;l.jsxs=a;d.exports=l;var c=d.exports,p,u=m;p=u.createRoot,u.hydrateRoot;function g(){return c.jsx("div",{className:"min-h-screen bg-red-500 flex items-center justify-center",children:c.jsx("h1",{className:"text-white text-4xl font-bold",children:"ТЕСТ - САЙТ РАБОТАЕТ!"})})}p(document.getElementById("root")).render(c.jsx(f.StrictMode,{children:c.jsx(g,{})}));
